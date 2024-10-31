9f2bb51b274d3341f82b1faaee3e28aa0380e065
Mariano Casco <mariano@mailbox.org>
2024-10-31 02:11:24 -0400

    Generate some hills with help of FastNoiseLite

--- chunk.cpp
+++ chunk.cpp
@@ -1,15 +1,32 @@
 #include "chunk.h"
 
+#include "FastNoiseLite.h"
+
+float get_noise(FastNoiseLite noise, float x, float y) {
+  return noise.GetNoise(x, y) / 2.0 + 0.5;
+}
+
 Chunk::Chunk(const int x, const int z) : cubes({}) {
   CubeTex tex{16, 16, 16, 16, 0, 32};
+  CubeTex water{202, 202, 202, 202, 202, 202};
+  FastNoiseLite noise;
 
   for (int i = 0; i < CHUNK_SIZE; i++)
     for (int j = 0; j < CHUNK_SIZE; j++) {
       const int ti = i - CHUNK_SIZE / 2;
       const int tj = j - CHUNK_SIZE / 2;
-      cubes.emplace_back(glm::vec3(static_cast<float>(x * CHUNK_SIZE + ti), 0,
-                                   static_cast<float>(z * CHUNK_SIZE + tj)),
-                         glm::ivec3(i, 0, j), tex);
+      const float _x = x * CHUNK_SIZE + ti;
+      const float _z = z * CHUNK_SIZE + tj;
+      float _y = get_noise(noise, _x, _z) +
+                 0.5f * get_noise(noise, 2 * _x, 2 * _z) +
+                 0.25f * get_noise(noise, 4 * _x, 4 * _z);
+      _y = _y / (1 + 0.5 + 0.25);
+      _y = std::round(_y * 14) / 14;
+      _y = std::round(13.0f * _y);
+      if (_y < 4)
+        cubes.emplace_back(glm::vec3(_x, _y, _z), glm::ivec3(i, _y, j), water);
+      else
+        cubes.emplace_back(glm::vec3(_x, _y, _z), glm::ivec3(i, _y, j), tex);
       chunk_map[i][j] = true;
     }
 }
